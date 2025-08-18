4369a8a6e581f9abdd2254f73b55f506f86217a1
IndalAwalaikal <indalawalaikal05@gmail.com>
2025-08-19 02:48:10 +0800

    update env

--- config/connected.go
+++ config/connected.go
@@ -2,35 +2,27 @@ package config
 
 import (
     "database/sql"
-    "fmt"
     "log"
     "os"
 
     _ "github.com/go-sql-driver/mysql"
 )
 
-func ConnectToDB() *sql.DB {
-    LoadEnv()
-
-    dbUser := os.Getenv("DB_USER")
-    dbPass := os.Getenv("DB_PASS")
-    dbHost := os.Getenv("DB_HOST")
-    dbPort := os.Getenv("DB_PORT")
-    dbName := os.Getenv("DB_NAME")
-
-    dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
-        dbUser, dbPass, dbHost, dbPort, dbName,
-    )
+func ConnectDB() *sql.DB {
+    dsn := os.Getenv("DATABASE_URL")
+    if dsn == "" {
+        log.Fatal("DATABASE_URL is not set")
+    }
 
     db, err := sql.Open("mysql", dsn)
     if err != nil {
-        log.Fatalf("Gagal membuka koneksi database: %v", err)
+        log.Fatal("Failed to open database:", err)
     }
 
-    if err = db.Ping(); err != nil {
-        log.Fatalf("Gagal terhubung ke database: %v", err)
+    if err := db.Ping(); err != nil {
+        log.Fatal("Database unreachable:", err)
     }
 
-    log.Println("Koneksi database berhasil")
+    log.Println("Database connected successfully")
     return db
 }
--- main.go
+++ main.go
@@ -12,25 +12,28 @@ import (
 )
 
 func main() {
+
     config.LoadEnv()
 
     utils.InitJWTSecret()
 
-    db := config.ConnectToDB()
+    db := config.ConnectDB()
     defer db.Close()
 
     mux := routes.Setup(db)
 
     handler := middleware.Recovery(middleware.Cors(mux))
 
-    port := os.Getenv("SERVER_PORT")
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = os.Getenv("SERVER_PORT")
+    }
     if port == "" {
         port = "8080"
     }
 
     fmt.Printf("Server berjalan di port %s\n", port)
-    err := http.ListenAndServe(fmt.Sprintf(":%s", port), handler)
-    if err != nil {
+    if err := http.ListenAndServe(":"+port, handler); err != nil {
         panic(fmt.Sprintf("Error menjalankan server: %v", err))
     }
 }
